5ff6cab3fcc9d0bb8dce96b410384746c5a526b0
LUKEPC\Luke <allhailluke@roantree.com>
2017-04-05 18:38:43 +0100

    Removed redundant files

--- adaptive.m
+++ adaptive.m
@@ -1,58 +1,69 @@
-function APP = adaptive(funcname, a, b, TOL, MAXINT)
+function APP = adaptive3(funcname, a, b, TOL, MAXINT)
   %APP = feval(funcname,1234); %placeholder til function is built, use feval rather
                                %than @ handle due to being messy in Octave
                                
-                               
-  % NOTE: v1,v2, ..., v8 aren't used, instead the values they are meant to
-  % hold will be assigned to the first element of the variables they represent
-  % (these variables have been made into arrays; 2nd element holds most up-to-date value, 1st
-  % holds the previous value)
   
   APP = 0;
   i = 1;
-  TOLi = (0, 10*TOL);
-  ai = (0, a);
-  hi = (0, ((b-a)*0.5)); % Initial step size half of full width
+  TOLi = [];
+  TOLi(1) = 10*TOL;
+  ai = [];
+  ai(1) = a;
+  hi = [];
+  hi(1) = ((b-a)*0.5); % Initial step size half of full width
   
   % Initial approximation Start
-  FAi = (0, feval(funcname,ai(2)));
-  FBi = (0, feval(funcname,b(2)));
-  FCi = (0, feval(funcname,(ai(2)+hi(2))));
-  Si = (0, hi(2)*(FAi(2) + 4*FCi(2) + FBi(2))*(1.0/3.0));
+  FAi = [];
+  FBi = [];
+  FCi = [];
+  Si = [];
+  Li = [];
+  FAi(1) = func_two(ai(end));
+  FBi(1) = func_two(b);
+  FCi(1) = func_two(ai(end)+hi(end));
+  Si(1) = hi(end) *(FAi(end) + 4*FCi(end) + FBi(end))*(1.0/3.0);
   % Initial approximation End
+  Li(1) = 1;
   
-  Li = (0,1);
   while(i>0)
-    FD = feval(funcname, (ai(2) +(hi(2)*0.5)));
-    FE = feval(funcname, (ai(2) + 3.0*(hi(2)*0.5)));
-    S1 = hi(2)*(1.0/6.0)*(FAi(2) + 4*FD + FCi(2));
-    S2 = hi(2)*(1.0/6.0)*(FCi(2) + 4*FE + FBi(2));
-    for VAR in (ai, FAi, FBi, FCi, hi, TOLi, Si, Li)  % Save current state
-      VAR(1) = VAR(2);
-    end
+    FD = func_two((ai(i) +(hi(i)*0.5)));
+    FE = func_two((ai(i) + 3.0*(hi(i) *0.5)));
+    S1 = hi(i)*(1.0/6.0)*(FAi(i) + 4*FD + FCi(i));
+    S2 = hi(i)*(1.0/6.0)*(FCi(i) + 4*FE + FBi(i));
+    v1=ai(i) ; v2=FAi(i) ; v4=FBi(i) ; v3 =FCi(i) ; v5=hi(i) ; v6=TOLi(i) ; v7=Si(i) ; v8=Li(i) ;
+    
+    
     i = i-1;  % Remove current level
-    if (abs(S1+S2-Si(1))<TOLi(1))
+    %disp(APP);
+    if (abs(S1+S2-v7)<v6)
       APP = APP + (S1+S2);
     else
-      if (Li(1)>=MAXINT)
+      if (v8>=MAXINT)
+        disp('shits fucked yo');
         error('MAXINT exceeded without achieving tolerance');
       else
+      
         i = i+1;
-        ai(2) = ai(1) + hi(1);
-        FAi(2) = FCi(1);
-        FCi(2) = FE;
-        FBi(2) = FBi(1);
-        hi(2) = hi(1)*0.5;
-        TOLi(2) = TOLi(1)*0.5;
-        Si(2) = S2;
-        Li(2) = Li(1) + 1;
+        
+        ai(i) = v1 + v5;
+        FAi(i) = v3;
+        FCi(i) = FE;
+        FBi(i) = v4;
+        hi(i) = v5*0.5;
+        TOLi(i) = v6*0.5;
+        Si(i) = S2;
+        Li(i) = v8 + 1;
+        
         i = i+1;
-        ai(2) = FCi(1);
-        FAi(2) = FAi(1);
-        FCi(2) = FD;
-        FBi(2) = FCi(1);
-        Si(2) = S1;
-        Li(2) = Li(1);
+        
+        ai(i) = v1;
+        FAi(i) = v2;
+        FCi(i) = FD;
+        FBi(i) = v3;
+        hi(i) = hi(i-1);
+        TOLi(i) = TOLi(i-1);
+        Si(i) = S1;
+        Li(i) = Li(i-1);
       end
     end
   end
